24744a8c7305be7d16d09a26b11ca698b99bb41d
Andreas Kloeckner <inform@tiker.net>
2022-09-21 22:43:14 -0500

    Spiff up example

--- example.py
+++ example.py
@@ -3,17 +3,24 @@ import starlark as sl
 glb = sl.Globals()
 mod = sl.Module()
 mod["a"] = 5
-ast = sl.parse("a.py", """
+
+ast = sl.parse("a.star", """
 z = 3
 z = 4
 
 def f(x):
-    return x*x - 5
+    z = 0
+    for i in range(13):
+        z += i*x
+    return x*x - 5 + z
+
 
 f(a - z)
 """)
+
 for lnt in ast.lint():
     print(lnt)
     print(lnt.serious)
+
 val = sl.eval(mod, ast, glb)
 print(val)
